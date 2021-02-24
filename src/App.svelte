fbfe7d0edad0d6eed8bc43eadb86bd47a4a14a1d
Vidur Khanal <vkhanal@caldwell.edu>
2021-02-24 11:24:49 -0500

    INITIAL COMMIT FROM SVELTE

---
+++ src/App.svelte
@@ -0,0 +1,42 @@
+<script lang="typescript">
+  export let name: string;
+
+  const toggleName = () => {
+    if (name === "vidur") {
+      name = "world";
+    } else {
+      name = "vidur";
+    }
+  };
+</script>
+
+<main>
+  <h1>Hello {name}!</h1>
+  <button on:click={toggleName}>Change My Name </button>
+  <p>
+    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+    how to build Svelte apps.
+  </p>
+</main>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
